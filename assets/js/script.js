9708d0e5b4fa34db5d42afc402938019b24a9263
Owen Lang <owen.lang146@gmail.com>
2023-08-02 12:46:35 -0500

    added consolelog

--- assets/js/script.js
+++ assets/js/script.js
@@ -300,9 +300,9 @@ function getLyrics(trackId){
       return
     }
     var lyricsArry =  response.lyrics.lines
-    console.log(lyricsArry)
-        for( var i = 0;i < lyricsArry.length; i++){
-            lyricPrompt = lyricPrompt + lyricsArry[i].words
+    console.log(lyricsArry);
+    for( var i = 0;i < lyricsArry.length; i++){
+        lyricPrompt = lyricPrompt + lyricsArry[i].words
       
     }
     showText(formatText(lyricPrompt))
